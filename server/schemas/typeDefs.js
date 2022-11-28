8e7fa63da1b23f76d56d2cf5a6e43748e64ca101
Racquel Munoz <racquelg85@hotmail.com>
2022-11-28 17:57:56 -0600

    added friend, thoughts, reactions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,6 +9,15 @@ const { gql } = require("apollo-server-express");
 //With this custom data type, we are able to instruct the thoughts query so that each thought that returns can include _id,
 //thoughtText, username, and reactionCount fields with their respective GraphQL scalars.
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -24,22 +33,27 @@ const typeDefs = gql`
     createdAt: String
     username: String
   }
-  type User {
-    _id: ID
-    username: String
-    email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
@@ -8,9 +9,12 @@ const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
+//This ensures that every request performs an authentication
+// check, and the updated request object will be passed to the resolvers as the context.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 const app = express();
